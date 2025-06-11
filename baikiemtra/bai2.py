0b99f787974a23c0b67d742b65e4f8ce33fd5a6f
Nguyenthienvinh <nguyenthienvinh2005@gmail.com>
2025-06-11 19:14:04 +0700

    Delete baikiemtra directory

--- baikiemtra/bai2.py
+++
@@ -1,7 +0,0 @@
-a=int(0)
-x=int(input())
-d =int (0)
-while a<x :
-    a+=3
-    d+=1
-print(d)
--- baikiemtra/bai4.py
+++
@@ -1,9 +0,0 @@
-a=[input(),input()]
-b=[]
-for i in range (2):
-    for j in range (len(a[i])):
-        b.append(a[i][j])
-b=set(b)
-b=list(b)
-print(b)
-
